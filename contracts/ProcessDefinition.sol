e2837da48985e06899f5eac99b9bb72db33372ca
Evgeny Shestakov <evgeny.shestakov@techusage.com>
2023-10-12 12:56:48 +0200

    add onlyOwner modifier

--- contracts/ProcessDefinition.sol
+++ contracts/ProcessDefinition.sol
@@ -1,3 +1,8 @@
+// SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.8.20 and less than 0.9.0
+pragma solidity ^0.8.20;
+
+
 contract ProcessDefinition {
     address public factory;
     // Mapping of started process instance IDs to their statuses.
@@ -30,19 +35,25 @@ contract ProcessDefinition {
         owner = _newOwner;
     }
 
+    function transferOwnership(address _newOwner) external onlyOwner {
+        owner = _newOwner;
+    }
+
     // Starts a new process instance.
-    function start(string memory processInstanceId) external {
+    function start(string memory processInstanceId) external payable {
         require(startedProcessInstances[processInstanceId] == ProcessInstanceStatus.Created, "Process instance already started.");
 
         startedProcessInstances[processInstanceId] = ProcessInstanceStatus.InProgress;
         list.push(processInstanceId);
         processInstanceOwners[processInstanceId] = msg.sender;
+
         emit ProcessInstanceStarted(processInstanceId);
     }
 
     // Updates the status of a process instance.
     function update(string memory processInstanceId, ProcessInstanceStatus status) external {
         require(startedProcessInstances[processInstanceId] != ProcessInstanceStatus.Created, "Process instance not started.");
+
         startedProcessInstances[processInstanceId] = status;
 
         if (status == ProcessInstanceStatus.Completed) {
