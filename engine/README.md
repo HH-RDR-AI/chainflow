1110f396987c216df9f402be06447f8db39810db
evahteev <evgeny@dex.guru>
2023-10-12 12:47:50 +0300

    adding deployment

--- engine/README.md
+++ engine/README.md
@@ -1,2 +1,21 @@
 # chainflow-engine
 ChainFlow Engine
+
+
+### Build docker:
+
+```
+docker buildx build --push --tag docker-registry.dexguru.biz/dex.guru/chainflow_engine/chainflow_engine:1 --platform=linux/arm64,linux/amd64 .
+```
+
+### Deploy using helm
+
+If you downloaded config from rancher
+```
+ export KUBECONFIG=~/Downloads/stage-k8s.yaml 
+ helm upgrade --install chainflow-engine ./helm/engine  --wait -n stage --set imageTag=1 --set appName=chainflow-engine --set kubeNamespace=stage
+```
+Available at:
+
+Application: https://chainflow-engine.dexguru.biz/camunda/app/welcome/default/
+API: https://chainflow-engine.dexguru.biz/engine-rest
--- engine/src/main/java/ai/hhrdr/chainflow/engine/CamundaApplication.java
+++ engine/src/main/java/ai/hhrdr/chainflow/engine/CamundaApplication.java
@@ -1,15 +1,51 @@
 package ai.hhrdr.chainflow.engine;
 
+import org.camunda.bpm.engine.rest.security.auth.ProcessEngineAuthenticationFilter;
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.servlet.Filter;
 
 @SpringBootApplication
+@EnableScheduling
+@Configuration
 @EnableProcessApplication("chainflow-engine")
-public class CamundaApplication {
+public class CamundaApplication extends SpringBootServletInitializer {
 
   public static void main(String... args) {
     SpringApplication.run(CamundaApplication.class, args);
+  }
+
+
+  /**
+   * Spring Servlet Initializer
+   *
+   * @param application application
+   * @return SpringApplicationBuilder
+   */
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(CamundaApplication.class);
+  }
+
+  @Bean
+  public FilterRegistrationBean authenticationFilter() {
+    FilterRegistrationBean registration = new FilterRegistrationBean();
+    Filter myFilter = new ProcessEngineAuthenticationFilter();
+    registration.setFilter(myFilter);
+    registration.addUrlPatterns("/engine-rest/*");
+    registration.addInitParameter("authentication-provider",
+            "org.camunda.bpm.engine.rest.security.auth.impl.HttpBasicAuthenticationProvider");
+    registration.setName("camunda-auth");
+    registration.setOrder(1);
+    return registration;
   }
 
 }
