f112fd28c9e100e733474af608be13fb1c9b1970
Evgeny Shestakov <evgeny.shestakov@techusage.com>
2023-10-18 13:01:24 +0200

    rework factory

--- contracts/ChainFlowFactory.sol
+++ contracts/ChainFlowFactory.sol
@@ -6,45 +6,60 @@ import "./ProcessDefinition.sol";
 
 
 contract ChainFlowFactory {
-    mapping(bytes32 => address) private deployedDefinitions;
-    bytes32[] private allDefinitions;
+    mapping(string => address) private deployedDefinitions;
+    string[] private allDefinitions;
     address public owner;
 
     constructor() {
         owner = msg.sender;
     }
 
-    event ProcessDefinitionCreated(bytes32 indexed bpmnHash, address processAddress);
+    event ProcessDefinitionCreated(string indexed definitionId, address processAddress);
 
-    function getContractAddressOfDefinition(bytes32 _defHash) public view returns (address) {
-        return deployedDefinitions[_defHash];
+    function getContractAddressOfDefinition(string memory definitionId) public view returns (address) {
+        return deployedDefinitions[definitionId];
     }
 
-    function getDefinitionCount() public view returns(uint) {
+    function getDefinitionCount() public view returns (uint) {
         return allDefinitions.length;
     }
 
-    function getAllDefinitions() public view returns (bytes32[] memory) {
+    function getAllDefinitions() public view returns (string[] memory) {
         // copy needs to prevent modifying
-        bytes32[] memory copyOfAllDefinitions = new bytes32[](allDefinitions.length);
+        string[] memory copyOfAllDefinitions = new string[](allDefinitions.length);
         for (uint i = 0; i < allDefinitions.length; i++) {
             copyOfAllDefinitions[i] = allDefinitions[i];
         }
         return copyOfAllDefinitions;
     }
 
-    function createDefinition(bytes32 _hash, uint _processPrice) external {
-        require(deployedDefinitions[_hash] == address(0), "Process definition already created!");
-        bytes memory bytecode = type(ProcessDefinition).creationCode;
-        bytes32 salt = keccak256(abi.encodePacked(_hash));
+    function createDefinition(
+        string memory definitionId
+    ) external {
+        require(deployedDefinitions[definitionId] == address(0), "Process definition already created!");
+        bytes memory bytecode = type(ProcessDefinitionToken).creationCode;
+        bytes memory constructorArgs = abi.encode(
+            msg.sender,
+            definitionId,
+            definitionId,
+            owner,
+            definitionId,
+            uint(500)
+        );
+        bytes32 salt = keccak256(abi.encodePacked(
+            definitionId,
+            block.timestamp,
+            block.prevrandao
+        ));
+        bytes memory deploymentBytecode = abi.encodePacked(bytecode, constructorArgs);
         address newProcessDefinition;
         assembly {
-            newProcessDefinition := create2(0, add(bytecode, 32), mload(bytecode), salt)
+            newProcessDefinition := create2(0, add(deploymentBytecode, 32), mload(deploymentBytecode), salt)
         }
-        ProcessDefinition(newProcessDefinition).initialize(msg.sender, owner, _processPrice);
-        deployedDefinitions[_hash] = newProcessDefinition;
-        allDefinitions.push(_hash);
-        emit ProcessDefinitionCreated(_hash, newProcessDefinition);
+        require(newProcessDefinition != address(0), "Failed to deploy process definition!");
+        deployedDefinitions[definitionId] = newProcessDefinition;
+        allDefinitions.push(definitionId);
+        emit ProcessDefinitionCreated(definitionId, newProcessDefinition);
     }
 
     function changeOwner(address _newOwner) external {
