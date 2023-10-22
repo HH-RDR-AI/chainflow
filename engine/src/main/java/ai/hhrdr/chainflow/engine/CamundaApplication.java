479897baf46f0016704a89fc0aec403181eb6ce8
evahteev <evgeny@dex.guru>
2023-10-22 15:53:13 +0300

    remove auth

--- engine/src/main/java/ai/hhrdr/chainflow/engine/CamundaApplication.java
+++ engine/src/main/java/ai/hhrdr/chainflow/engine/CamundaApplication.java
@@ -1,18 +1,15 @@
 package ai.hhrdr.chainflow.engine;
 
-import org.camunda.bpm.engine.rest.security.auth.ProcessEngineAuthenticationFilter;
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import javax.servlet.Filter;
 
 @SpringBootApplication
 @EnableScheduling
@@ -43,18 +40,18 @@ public class CamundaApplication extends SpringBootServletInitializer {
   public static PropertySourcesPlaceholderConfigurer propertyConfigurer() {
     return new PropertySourcesPlaceholderConfigurer();
   }
-
-  @Bean
-  public FilterRegistrationBean authenticationFilter() {
-    FilterRegistrationBean registration = new FilterRegistrationBean();
-    Filter myFilter = new ProcessEngineAuthenticationFilter();
-    registration.setFilter(myFilter);
-    registration.addUrlPatterns("/engine-rest/*");
-    registration.addInitParameter("authentication-provider",
-            "org.camunda.bpm.engine.rest.security.auth.impl.HttpBasicAuthenticationProvider");
-    registration.setName("camunda-auth");
-    registration.setOrder(1);
-    return registration;
-  }
+//
+//  @Bean
+//  public FilterRegistrationBean authenticationFilter() {
+//    FilterRegistrationBean registration = new FilterRegistrationBean();
+//    Filter myFilter = new ProcessEngineAuthenticationFilter();
+//    registration.setFilter(myFilter);
+//    registration.addUrlPatterns("/engine-rest/*");
+//    registration.addInitParameter("authentication-provider",
+//            "org.camunda.bpm.engine.rest.security.auth.impl.HttpBasicAuthenticationProvider");
+//    registration.setName("camunda-auth");
+//    registration.setOrder(1);
+//    return registration;
+//  }
 
 }
---
+++ engine/src/main/java/ai/hhrdr/chainflow/engine/config/CorsConfig.java
@@ -0,0 +1,34 @@
+package ai.hhrdr.chainflow.engine.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+
+    private static final long MAXAGE = 3600L;
+
+    @Bean
+    public FilterRegistrationBean corsFilterRegistrationBean() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.applyPermitDefaultValues();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList("*"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowedMethods(Arrays.asList("*"));
+        config.setExposedHeaders(Arrays.asList("content-length"));
+        config.setMaxAge(MAXAGE);
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+}
