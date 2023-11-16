b6f5961ff205d4e5c86ec3ae4b47085f12596244
Rodion Kharabet <pensioneer4ik@gmail.com>
2023-11-16 12:56:37 +0200

    update config

--- gui/next.config.js
+++ gui/next.config.js
@@ -32,6 +32,13 @@ const nextConfig = {
   //     },
   //   ];
   // },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.externals.push({ bufferutil: "bufferutil", "utf-8-validate": "utf-8-validate" });
+    }
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
