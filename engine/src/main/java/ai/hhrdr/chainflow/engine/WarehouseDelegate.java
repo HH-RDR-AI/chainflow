9ff9d2afe53f9814ac3825e944dbde0cd52fae08
Vitaly Vakhteev <vitaly.v@techusage.com>
2023-12-27 04:06:26 -0800

    Progress with Dashboard publishing

--- engine/src/main/java/ai/hhrdr/chainflow/engine/WarehouseDelegate.java
+++ engine/src/main/java/ai/hhrdr/chainflow/engine/WarehouseDelegate.java
@@ -1,8 +1,26 @@
 package ai.hhrdr.chainflow.engine;
 
+import camundajar.impl.com.google.gson.Gson;
+import org.apache.hc.client5.http.async.methods.SimpleHttpRequest;
+import org.apache.hc.client5.http.async.methods.SimpleHttpResponse;
+import org.apache.hc.client5.http.async.methods.SimpleRequestBuilder;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.impl.async.CloseableHttpAsyncClient;
+import org.apache.hc.client5.http.impl.async.HttpAsyncClients;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.http.io.entity.StringEntity;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Future;
 import java.util.logging.Logger;
 
 /**
@@ -10,8 +28,15 @@ import java.util.logging.Logger;
  * illustrating how a Java Delegate can be used
  * from within a BPMN 2.0 Service Task.
  */
+@Component("warehouse")
 public class WarehouseDelegate implements JavaDelegate {
- 
+
+  @Value("${camunda.bpm.api.url}")
+  private String apiURL;
+
+  @Value("${camunda.bpm.api.key}")
+  private String apiKey;
+
   private final Logger LOGGER = Logger.getLogger(WarehouseDelegate.class.getName());
   
   public void execute(DelegateExecution execution) throws Exception {
@@ -28,8 +53,26 @@ public class WarehouseDelegate implements JavaDelegate {
 
     // Get ID, type of publication (dashboard, query, etc.) make post request to change is_draft to false
     // Precondition: Complete is successful
-    //    execution.getVariables();
+    Gson gson = new Gson();
+    Map<String, Object> jsonMap = new HashMap<>();
+    jsonMap.put("is_draft", false);
+    String json = gson.toJson(jsonMap);
+    if (execution.getProcessDefinitionId().contains("warehouse_query_review")) {
+      try (CloseableHttpClient client = HttpClients.createDefault()) {
+        HttpPost httpPost = new HttpPost((apiURL + "/api/dashboards/") + execution.getVariable("dashboard_id"));
+        httpPost.setEntity(new StringEntity(json));
+        httpPost.setHeader("Content-Type", "application/json");
+        httpPost.setHeader("Authorization", apiKey);
+        try (CloseableHttpResponse response = client.execute(httpPost)) {
+          LOGGER.info("Response Code: " + response.getCode());
+          LOGGER.info("Response Body: " + EntityUtils.toString(response.getEntity()));
+        }
+      }
+    }
+    else if (execution.getProcessDefinitionId().contains("warehouse_real_query_review")) {
 
+    }
   }
 
 }
+
