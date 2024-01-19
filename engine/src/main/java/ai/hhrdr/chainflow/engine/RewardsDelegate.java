82dcd32199852912393697d0ac4edd455c878bc6
Vitaly Vakhteev <vitaly.v@techusage.com>
2024-01-18 18:22:08 -0800

    change design

--- engine/src/main/java/ai/hhrdr/chainflow/engine/RewardsDelegate.java
+++ engine/src/main/java/ai/hhrdr/chainflow/engine/RewardsDelegate.java
@@ -7,7 +7,6 @@ import org.camunda.bpm.engine.impl.persistence.entity.ExecutionEntity;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.io.FileReader;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -45,16 +44,6 @@ public class RewardsDelegate implements JavaDelegate {
          */
         // Todo: branch rewards delegate based on current task stage; one branch for posting unclaimed, one for claimed
         // Todo: determine rewards for different processes
-        Gson gson = new Gson();
-        HttpClient amount_client = HttpClient.newHttpClient();
-        String amount_response = getRequest(amount_client, execution);
-        assert amount_response != null;
-        JsonElement jsonElement = JsonParser.parseString(amount_response);
-        JsonObject jsonObject = jsonElement.getAsJsonObject();
-        JsonArray totals = jsonObject.getAsJsonArray();
-        String amount = String.valueOf(totals.get(0));
-        String amount_unclaimed = String.valueOf(totals.get(1));
-
         String json =
                 "{\"definition_key\": " + ((ExecutionEntity) execution).getProcessDefinition().getKey() + ", "
                 + "\"process_instance_id\": " + execution.getProcessInstanceId() + ", ";
@@ -62,8 +51,8 @@ public class RewardsDelegate implements JavaDelegate {
         switch (((ExecutionEntity) execution).getProcessDefinition().getKey()) {
             case "warehouse_dashboard_review":
                 try {
-                    json += "\"amount\": " + amount + ", "
-                            + "\"amount_unclaimed\": " + (amount_unclaimed + 50) + "}"; // placeholder
+                    json += "\"amount\": " + 0 + ", "
+                            + "\"amount_unclaimed\": " + 25 + "}"; // placeholder
                     sendRequest(client, execution, json);
                 }
                 catch (Exception e) {
@@ -72,8 +61,8 @@ public class RewardsDelegate implements JavaDelegate {
                 break;
             case "warehouse_query_review":
                 try {
-                    json += "\"amount\": " + amount + ", "
-                            + "\"amount_unclaimed\": " + (amount_unclaimed + 100) + "}"; // placeholder
+                    json += "\"amount\": " + 0 + ", "
+                            + "\"amount_unclaimed\": " + 50 + "}"; // placeholder
                     sendRequest(client, execution, json);
                 }
                 catch (Exception e) {
@@ -96,24 +85,6 @@ public class RewardsDelegate implements JavaDelegate {
             LOGGER.info("Publish Response Code: " + response.statusCode() + "\nPublish Response Body: " + response.body());
         } catch (IOException | InterruptedException e) {
             LOGGER.severe("Error while sending HTTP request: " + e.getMessage());
-        }
-    }
-
-    private String getRequest(HttpClient client, DelegateExecution execution) {
-        try {
-            HttpRequest req = HttpRequest
-                    .newBuilder()
-                    .uri(URI.create(apiURL + "/api/points/" + execution.getVariable("user_id")))
-                    .header("Content-Type", "application/json")
-                    .header("Authorization", apiKey)
-                    .GET()
-                    .build();
-            HttpResponse<String> response = client.send(req, HttpResponse.BodyHandlers.ofString());
-            LOGGER.info("Publish Response Code: " + response.statusCode() + "\nPublish Response Body: " + response.body());
-            return response.body();
-        } catch (IOException | InterruptedException e) {
-            LOGGER.severe("Error while sending HTTP request: " + e.getMessage());
-            return null;
         }
     }
 }
