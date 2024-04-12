566dccebfcffb35717c97317baf8e2d3a30c332b
Rodion Kharabet <pensioneer4ik@gmail.com>
2024-04-12 12:21:36 +0300

    fix build warnings

--- gui/next.config.js
+++ gui/next.config.js
@@ -1,23 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/dashboard',
+  basePath: "/dashboard",
   experimental: {
-    serverComponentsExternalPackages: [
-      "hammerjs",
-    ],
+    serverComponentsExternalPackages: ["hammerjs"],
   },
   rewrites: async () => {
     return [
       {
         source: "/app/engine/:path*",
         destination: "https://chainflow-engine.dexguru.biz/engine-rest/:path*",
-      }
+      },
     ];
   },
   webpack: (config, { isServer }) => {
     if (!isServer) {
-      config.externals.push({ bufferutil: "bufferutil", "utf-8-validate": "utf-8-validate" });
+      config.externals.push({
+        bufferutil: "bufferutil",
+        "utf-8-validate": "utf-8-validate",
+      });
     }
+    config.externals.push("pino-pretty", "encoding");
 
     return config;
   },
