94ca7cb79ebc17bdf668e73859d0856dc3563b47
Rodion Kharabet <pensioneer4ik@gmail.com>
2024-04-25 12:41:07 +0300

    add verifyMessage logic from wagmi core

--- gui/src/config/siwe.ts
+++ gui/src/config/siwe.ts
@@ -3,6 +3,11 @@ import { createSIWEConfig } from '@web3modal/siwe'
 import type { SIWECreateMessageArgs, SIWESession, SIWEVerifyMessageArgs } from '@web3modal/siwe'
 import client, { User, UserProfile } from '../camunda/client';
 import { SessionStorageEnumKeys } from '../constants';
+import { verifyMessage as wagmiVerifyMessage } from '@wagmi/core'
+import { config } from './wagmi';
+import { AccountController } from '@web3modal/core'
+import { Hex } from 'viem';
+
 
 function generateNonce(length: number): string {
     const array = new Uint8Array(length / 2);
@@ -44,7 +49,11 @@ async function getSession(): Promise<SIWESession> {
 /* Use your SIWE server to verify if the message and the signature are valid */
 async function verifyMessage({ message, signature }: SIWEVerifyMessageArgs) {
     try {
-        const isValid = true
+        const { address } = AccountController.state
+        if (!address) {
+            throw new Error('address is not defined')
+        }
+        const isValid = await wagmiVerifyMessage(config, { address: address as `0x${string}`, message, signature: signature as Hex })
 
         return isValid
     } catch (error) {
@@ -57,6 +66,29 @@ async function signOut(): Promise<boolean> {
     return true
 }
 
+async function onSignIn(session?: SIWESession): Promise<void> {
+    if (!session) {
+        session = await getSession()
+    }
+    let user: User | undefined = undefined
+    try {
+        user = await client.getUser(session.address)
+    }
+    catch (e) {
+        console.error(e)
+        const userData: UserProfile = {
+            profile: {
+                id: session.address,
+            }
+        }
+        await client.createUser(userData);
+        await client.createGroupMember("walletUsers", userData.profile.id)
+        user = userData.profile
+    } finally {
+        user && sessionStorage.setItem(SessionStorageEnumKeys.userData, JSON.stringify(user))
+    }
+}
+
 /* Create a SIWE configuration object */
 export const siweConfig = createSIWEConfig({
     createMessage,
@@ -64,27 +96,5 @@ export const siweConfig = createSIWEConfig({
     getSession,
     verifyMessage,
     signOut,
-    onSignIn: async (session) => {
-        if (!session) {
-            session = await getSession()
-        }
-        let user: User | undefined = undefined
-        try {
-            user = await client.getUser(session.address)
-        }
-        catch (e) {
-            console.error(e)
-            const userData = {
-                profile: {
-                    id: session.address,
-                }
-            }
-            await client.createUser(userData);
-            await client.createGroupMember("walletUsers", userData.profile.id)
-            user = userData.profile
-        } finally {
-            sessionStorage.setItem(SessionStorageEnumKeys.userData, JSON.stringify(user))
-        }
-
-    },
+    onSignIn
 })
